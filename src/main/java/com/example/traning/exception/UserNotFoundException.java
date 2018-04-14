902eaf0b95ea45782ac00d1029625d80aa1c89b8
sahil Dwivedi <sahil@sahil.dwivedi>
2018-04-14 12:09:46 +0530

    removing all files

--- src/main/java/com/example/traning/exception/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.traning.exception;
-
-public class UserNotFoundException extends Exception {
-	
-	public UserNotFoundException(String exception) {
-	    super(exception);
-	  }
-
-}
--- src/main/java/com/example/traning/repository/TransectionRepository.java
+++
@@ -1,6 +0,0 @@
-/*package com.example.traning.repository;
-
-public class TransectionRepository {
-
-}
-*/
